ef59ffc7ae558e8f793e8ffc3a8daae9c929be06
Murali <muralikrishna1710@gmail.com>
2017-02-02 21:04:22 -0500

    Initial commit for our first Spring boot project

---
+++ src/main/java/com/fullstackdemo/FullstackkdemoApplication.java
@@ -0,0 +1,12 @@
+package com.fullstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackkdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackkdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fullstackdemo/FullstackkdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fullstackdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullstackkdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
